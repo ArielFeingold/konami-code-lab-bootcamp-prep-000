153882b40d7a4b0da5db0250de378c3df5f35eb2
Ariel Feingold <feingold.ariel@gmail.com>
2018-01-08 19:46:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 const body = document.querySelector(`body`);
 let index = 0;
 
-function int(e){
+function init(e){
 	body.addEventListener(`keydown`, function(e){
 
 		const key = parseInt(e.detail || e.which); 
